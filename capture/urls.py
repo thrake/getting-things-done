abdef3598e1f24b358f1ff57944fa175c3385a75
thrake <morten.lamp@gmail.com>
2019-04-24 21:15:25 +0200

    Day 2 – Creating Templates and first HTML Outputs

--- capture/urls.py
+++ capture/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /capture/
     path('', views.index, name='index'),
+    # ex: /capture/5/
+    path('<int:capture_id>/', views.detail, name='detail'),
+    # ex: /capture/all
+    path('all/', views.all, name='all'),
 ]
 
--- capture/views.py
+++ capture/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+
+from .models import Capture
 
 # Create your views here.
 def index(request):
+    all_capture_list = Capture.objects.order_by('-capture_date')[:5]
+    context = {'all_capture_list': all_capture_list}
+    return render(request, 'capture/index.html', context)
+    
     return HttpResponse("Hello, world. You're at the capture index.")
+
+def detail(request, capture_id):
+        text = get_object_or_404(Capture, id=capture_id)
+        return render(request, 'capture/detail.html', {'text': text})    
+
+def all(request):
+    all_capture_list = Capture.objects.order_by('-capture_date')[:5]
+    output = ', '.join([c.capture_text for c in all_capture_list])
+    return HttpResponse(output)
