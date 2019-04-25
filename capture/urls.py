6b39985138a6ece52afb3dd417d73b47492ac359
thrake <morten.lamp@gmail.com>
2019-04-25 20:08:05 +0200

    Created Form and POST functionality

--- capture/urls.py
+++ capture/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'capture'
 urlpatterns = [
     # ex: /capture/
     path('', views.index, name='index'),
@@ -9,5 +10,7 @@ urlpatterns = [
     path('<int:capture_id>/', views.detail, name='detail'),
     # ex: /capture/all
     path('all/', views.all, name='all'),
+    # ex: /capture/capture
+    path('capture/', views.capture, name='capture'),
 ]
 
--- capture/views.py
+++ capture/views.py
@@ -1,21 +1,32 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.urls import reverse
+from django.utils import timezone
 
 from .models import Capture
 
 # Create your views here.
 def index(request):
-    all_capture_list = Capture.objects.order_by('-capture_date')[:5]
+    all_capture_list = Capture.objects.order_by('-capture_date')[:20]
     context = {'all_capture_list': all_capture_list}
     return render(request, 'capture/index.html', context)
-    
-    return HttpResponse("Hello, world. You're at the capture index.")
 
 def detail(request, capture_id):
-        text = get_object_or_404(Capture, id=capture_id)
-        return render(request, 'capture/detail.html', {'text': text})    
+    capture = get_object_or_404(Capture, id=capture_id)
+    return render(request, 'capture/detail.html', {'capture': capture})    
 
 def all(request):
-    all_capture_list = Capture.objects.order_by('-capture_date')[:5]
+    all_capture_list = Capture.objects.order_by('-capture_date')[:20]
     output = ', '.join([c.capture_text for c in all_capture_list])
     return HttpResponse(output)
+
+def capture(request):
+    print("capture started")
+    textinput = request.POST['capture']
+    c = Capture(capture_text=textinput, capture_date=timezone.now())
+    c.save()
+    print(c, "saved!")
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('capture:index', args=None))
