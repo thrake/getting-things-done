7f4dee165fb37ac7c7591e68560472577e12118b
thrake <morten.lamp@gmail.com>
2019-04-29 21:49:52 +0200

    Adding categories and status codes. Favicon added.

--- capture/models.py
+++ capture/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 class Capture(models.Model):
     capture_text = models.CharField(max_length=500)
     capture_date = models.DateTimeField('date captured')
+    category = models.SmallIntegerField()
+    status = models.SmallIntegerField()
     def __str__(self):
         return self.capture_text
     def was_published_recently(self):
