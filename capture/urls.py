8030f846843dd2cf2e7a839673abb52cbf7aa8f8
thrake <morten.lamp@gmail.com>
2019-05-04 12:33:16 +0200

    Delete Function, Font Awesome, Navigation, Filters

--- capture/urls.py
+++ capture/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path('all/', views.all, name='all'),
     # ex: /capture/capture
     path('capture/', views.capture, name='capture'),
+    # ex: /capture/bin
+    path('capture/bin', views.paper_bin, name='paper_bin'),
+    # ex; /capture/delete/5
+    path('delete/<int:capture_id>', views.delete, name='delete'),
 ]
 
--- capture/views.py
+++ capture/views.py
@@ -7,10 +7,25 @@ from .models import Capture
 
 # Create your views here.
 def index(request):
-    all_capture_list = Capture.objects.order_by('-capture_date')
-    amount = Capture.objects.filter(status=0).count()
-    context = {'all_capture_list': all_capture_list,
-               'amount': amount,}
+    all_capture_list = Capture.objects.filter(status=0, category=0).order_by('-capture_date')
+    amount = Capture.objects.filter(status=0, category=0).count()
+    bin_amount = Capture.objects.filter(category=8).count()
+    context = {
+        'all_capture_list': all_capture_list,
+        'amount': amount,
+        'bin_amount': bin_amount,
+               }
+    return render(request, 'capture/index.html', context)
+
+def paper_bin(request):
+    paper_bin_list = Capture.objects.filter(category=8).order_by('-capture_date')
+    amount = Capture.objects.filter(status=0, category=0).count()
+    bin_amount = Capture.objects.filter(category=8).count()
+    context = {
+        'paper_bin_list': paper_bin_list,
+        'amount': amount,
+        'bin_amount': bin_amount,
+               }
     return render(request, 'capture/index.html', context)
 
 def detail(request, capture_id):
@@ -25,6 +40,7 @@ def detail(request, capture_id):
     context = {'capture': capture}
     return render(request, 'capture/detail.html', context)    
 
+# all is non-functional atm
 def all(request):
     all_capture_list = Capture.objects.order_by('-capture_date')
     output = ', '.join([c.capture_text for c in all_capture_list])
@@ -40,3 +56,13 @@ def capture(request):
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
     return HttpResponseRedirect(reverse('capture:index', args=None))
+
+def delete(request,capture_id =None):
+    object = Capture.objects.get(id=capture_id)
+    if object.category == 8:
+        object.delete()
+        return HttpResponseRedirect(reverse('capture:paper_bin', args=None))
+    else:
+        object.category = 8
+        object.save()
+        return HttpResponseRedirect(reverse('capture:index', args=None))
